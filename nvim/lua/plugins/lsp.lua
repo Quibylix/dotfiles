535373fc77cb541a585e8364d97aee762a1b755b
Quibylix <barraza.fredimanuel@gmail.com>
2025-05-09 21:58:02 -0600

    feat: add lsp ui plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,13 +20,19 @@ return {
           vim.lsp.buf.format()
         end
       })
-
-      vim.keymap.set("n", "<leader>rn", function()
-        vim.lsp.buf.rename();
-      end)
-      vim.keymap.set("n", "<leader>ca", function()
-        vim.lsp.buf.code_action();
-      end)
     end
+  },
+  {
+    "jinzhongjia/LspUI.nvim",
+    event = "LspAttach",
+    branch = "main",
+    opts = {},
+    keys = {
+      { "<leader>rn", "<cmd>LspUI rename<cr>",      { "n", "v" } },
+      { "K",          "<cmd>LspUI hover<cr>",       { "n", "v" } },
+      { "<leader>ca", "<cmd>LspUI code_action<cr>", { "n", "v" } },
+      { "<leader>rf", "<cmd>LspUI reference<cr>",   { "n", "v" } },
+    },
+    version = "a8f0720168e48561d0f22c418a225ca8933fc258"
   }
 }
